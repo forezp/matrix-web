36782f71773695d1de4ae53127874b60a490ae31
“ <124746406@qq.com>
2019-08-21 18:18:40 +0800

    工作流

---
+++ matrix-web-admin/src/views/workflow/process/index.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="app-container">
+    <div class="block">
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <el-input v-model="listQuery.category" placeholder="请输入分类名称"></el-input>
+        </el-col>
+        <el-col :span="6">
+          <el-button type="success" icon="el-icon-search" @click.native="search">{{ $t('button.search') }}</el-button>
+          <el-button type="primary" icon="el-icon-refresh" @click.native="reset">{{ $t('button.reset') }}</el-button>
+        </el-col>
+      </el-row>
+      <br>
+      <el-row>
+        <el-col :span="24">
+          <el-button type="success" icon="el-icon-plus" @click.native="add">{{ $t('button.add') }}</el-button>
+          <el-button type="primary" icon="el-icon-edit" @click.native="edit">{{ $t('button.edit') }}</el-button>
+        </el-col>
+      </el-row>
+    </div>
+
+    
+    public String processonDefinitionId;
+    public String key;
+    public String name;
+    public Integer revision;
+    public Long deploymentTime;
+    public String xmlName;
+    public String picName;
+    public String deploymentId;
+    public Boolean suspend;
+    public String description;
+
+    <el-table :data="list" v-loading="listLoading" element-loading-text="Loading" border fit highlight-current-row
+              @current-change="handleCurrentChange">
+      <el-table-column label="流程ID">
+        <template slot-scope="scope">
+          {{scope.row.deploymentId}}
+        </template>
+      </el-table-column>
+      <el-table-column label="分类名称">
+        <template slot-scope="scope">
+          {{scope.row.category}}
+        </template>
+      </el-table-column>
+      <el-table-column label="名称">
+        <template slot-scope="scope">
+          {{scope.row.name}}
+        </template>
+      </el-table-column>
+      <el-table-column label="父分类ID">
+        <template slot-scope="scope">
+          {{scope.row.pcategoryId}}
+        </template>
+      </el-table-column>
+
+      <el-table-column label="创建时间">
+        <template slot-scope="scope">
+          {{scope.row.createTime}}
+        </template>
+      </el-table-column>
+
+    </el-table>
+
+    <el-dialog
+      :title="formTitle"
+      :visible.sync="formVisible"
+      width="70%">
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-row>
+          <el-col :span="12">
+            <template>
+              是否为父分类
+              <el-radio-group :disabled= "rationDisabled" v-model="radio" @change="rationchangeHandler">
+                <el-radio class="radio" label="1">是</el-radio>
+                <el-radio class="radio" label="2">否</el-radio>
+              </el-radio-group>
+            </template>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="分类ID" prop="categoryId">
+              <el-input v-model="form.categoryId" :disabled="groupIdInputDisabled"></el-input>
+            </el-form-item>
+          </el-col>
+
+          <el-col :span="12">
+            <el-form-item label="分类名称" prop="categoryName">
+              <el-input v-model="form.categoryName"></el-input>
+            </el-form-item>
+          </el-col>
+
+          <el-col :span="12" v-if="pCategoryIdDisabled">
+            <el-form-item   label="父分类ID" prop="pCategoryId">
+              <el-input v-model="form.pCategoryId" :disabled="pCategoryIdDisabled"></el-input>
+            </el-form-item>
+          </el-col>
+
+          <el-col :span="12" v-if="!pCategoryIdDisabled">
+            <el-form-item label="父分类ID" >
+              <el-input
+                placeholder="请选择父分类ID"
+                v-model="form.pCategoryId"
+                @click.native="showTree = !showTree"
+              >
+              </el-input>
+              <el-tree v-if="showTree"
+                       empty-text="暂无数据"
+                       :expand-on-click-node="false"
+                       :data="pCategoryData"
+                       :props="defaultProps"
+                       @node-click="handleNodeClick"
+                       class="input-tree">
+              </el-tree>
+            </el-form-item>
+          </el-col>
+
+        </el-row>
+        <el-form-item>
+          <el-button type="primary" @click="save">{{ $t('button.submit') }}</el-button>
+          <el-button @click.native="formVisible = false">{{ $t('button.cancel') }}</el-button>
+        </el-form-item>
+
+      </el-form>
+    </el-dialog>
+    <el-pagination
+      background
+      layout="total, sizes, prev, pager, next, jumper"
+      :page-sizes="[10, 20, 50, 100,500]"
+      :page-size="listQuery.pageSize"
+      :total="totalCount"
+      @size-change="changeSize"
+      @current-change="fetchPage"
+      @prev-click="fetchPrev"
+      @next-click="fetchNext">
+    </el-pagination>
+  </div>
+</template>
+
+<script src="./process.js"></script>
+
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "src/styles/common.scss";
+</style>
+
---
+++ matrix-web-admin/src/views/workflow/process/process.js
@@ -0,0 +1,210 @@
+import {addCategory, updateCategory, categoryList} from '@/api/workflow/category'
+
+export default {
+  data() {
+    return {
+      isUpdate: false,
+      formVisible: false,
+      groupIdInputDisabled: false,
+      formTitle: '添加任务',
+      isAdd: true,
+      form: {
+        id: '',
+        categoryId: '',
+        categoryName: '',
+        pCategoryId: ''
+      },
+      rules: {
+        categoryId: [
+          {required: true, message: '请输入分类ID', trigger: 'blur'},
+          {min: 1, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur'}
+        ],
+        categoryName: [
+          {required: true, message: '请输入分类名', trigger: 'blur'}
+        ]
+
+      },
+      showTree: false,
+      defaultProps: {
+        id: 'id',
+        label: 'categoryName',
+        children: 'children'
+      },
+      listQuery: {
+        category: undefined,
+        page: 1,
+        pageSize: 100
+      },
+      listPCategoryQuery: {
+        pCategoryId: 0,
+        page: 1,
+        pageSize: 100
+      },
+      totalCount: 0,
+      list: null,
+      pCategoryData: null,
+      listLoading: true,
+      selRow: {},
+      radio: '2',
+      pCategoryIdDisabled: false,
+      rationDisabled: false
+    }
+  },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+  created() {
+    this.init()
+  },
+  methods: {
+    init() {
+      this.fetchData()
+      this.fetchPCategoryData()
+    },
+    fetchData() {
+      this.listLoading = true
+      categoryList(this.listQuery).then(response => {
+        this.list = response.data.list
+        this.listLoading = false
+        this.totalCount = response.data.totalCount
+      })
+    },
+    fetchPCategoryData() {
+      categoryList(this.listPCategoryQuery).then(response => {
+        this.pCategoryData = response.data.list
+      })
+    },
+    rationchangeHandler(value) {
+      console.info(value)
+      if (value === '1') {
+        console.info(value)
+        this.pCategoryIdDisabled = true
+        this.form.pCategoryId = 0
+      } else {
+        this.pCategoryIdDisabled = false
+        this.form.pCategoryId = ''
+      }
+    },
+    search() {
+      this.fetchData()
+    },
+    reset() {
+      this.listQuery.categoryId = ''
+      this.listQuery.pCategoryId = ''
+      this.listQuery.categoryName = ''
+      this.fetchData()
+    },
+    handleFilter() {
+      this.listQuery.page = 1
+      this.getList()
+    },
+    handleClose() {
+
+    },
+    handleCurrentChange(currentRow, oldCurrentRow) {
+      this.selRow = currentRow
+    },
+    resetForm() {
+      this.form = {}
+    },
+    add() {
+      this.resetForm()
+      this.formTitle = '添加分类'
+      this.formVisible = true
+      this.isAdd = true
+      this.isUpdate = false
+      this.groupIdInputDisabled = false
+      this.pCategoryIdDisabled = false
+      this.rationDisabled = false
+    },
+    fetchNext() {
+      this.listQuery.page = this.listQuery.page + 1
+      this.fetchData()
+    },
+    fetchPrev() {
+      this.listQuery.page = this.listQuery.page - 1
+      this.fetchData()
+    },
+    fetchPage(page) {
+      this.listQuery.page = page
+      this.fetchData()
+    },
+    changeSize(pageSize) {
+      this.listQuery.pageSize = pageSize
+      this.fetchData()
+    },
+    save() {
+      var self = this
+      this.$refs['form'].validate((valid) => {
+        if (valid) {
+          if (this.isUpdate) {
+            updateCategory({
+              id: self.form.id,
+              categoryName: self.form.categoryName
+            }).then(response => {
+              console.log(response)
+              this.$message({
+                message: '提交成功',
+                type: 'success'
+              })
+              this.fetchData()
+              this.formVisible = false
+            })
+          } else {
+            addCategory({
+              id: self.form.id,
+              categoryId: self.form.categoryId,
+              categoryName: self.form.categoryName,
+              pCategoryId: self.form.pCategoryId
+            }).then(response => {
+              console.log(response)
+              this.$message({
+                message: '提交成功',
+                type: 'success'
+              })
+              this.fetchData()
+              this.formVisible = false
+            })
+          }
+        } else {
+          return false
+        }
+      })
+    },
+    checkSel() {
+      if (this.selRow && this.selRow.id) {
+        return true
+      }
+      this.$message({
+        message: '请选中操作项',
+        type: 'warning'
+      })
+      return false
+    },
+    edit() {
+      if (this.checkSel()) {
+        this.isAdd = false
+        this.form = this.selRow
+        this.formTitle = '修改分类'
+        this.formVisible = true
+        this.groupIdInputDisabled = true
+        this.isUpdate = true
+        this.form.pCategoryId = this.selRow.pcategoryId
+        this.pCategoryIdDisabled = true
+        this.rationDisabled = true
+      }
+    },
+    handleNodeClick(data, node) {
+      console.log(data)
+      this.form.pCategoryId = data.categoryId
+      this.showTree = false
+    }
+  }
+}
